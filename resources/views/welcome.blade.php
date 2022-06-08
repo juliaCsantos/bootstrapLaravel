b5024e31af75f80adca10581265c5f583ecc9e4b
juliaCsantos <julia.santos456@etec.sp.gov.br>
2022-06-08 09:02:38 -0300

    Bootstrap no Laravel

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bootrstrap no Laravel</title>
+    <link rel="stylesheet" href="{{asset('css/bootstrap.css')}}">
+    <script src="{{asset('js/jquery.js')}}"></script>
+    <script src="{{asset('js/bootstrap.js')}}"></script>
+</head>
+<body>
+    <h1>Utilizando Bootrstrap</h1>
+    <button class="btn btn-lg btn-success">Bootstrap</button>
+</body>
+</html>
